8bf360701004137e774765adbbb893756c9f250e
BeibarysShayakhmetov <shazam.chik88@mail.ru>
2026-02-19 16:07:56 +0500

    datetime operations

---
+++ datetime/2.py
@@ -0,0 +1,5 @@
+import datetime
+
+x = datetime.datetime(2026, 4, 5)
+
+print(x)
---
+++ datetime/4.py
@@ -0,0 +1,5 @@
+import datetime
+start = datetime.datetime(2025, 4, 5)
+end = datetime.datetime.now()
+diff = end - start
+print(diff)
