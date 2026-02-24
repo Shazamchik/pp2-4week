3418e333c38d1e55f9d97803e254126e43dbc98d
BeibarysShayakhmetov <shazam.chik88@mail.ru>
2026-02-24 19:58:01 +0500

    fixed mistake in json ex

--- exercises/1.py
+++ exercises/1.py
@@ -2,12 +2,9 @@ import json
 with open("sample-data.json", "r") as file:
     data = json.load(file)
 print("Interface status")
-print("=" * 50)
+print("=" * 85)
+
 for x in data["imdata"]:
     att = x["l1PhysIf"]["attributes"]
     if att["id"] in ["eth1/33", "eth1/34", "eth1/35"]:
-        print("DN:", att["dn"])
-        print("Description:", att["descr"])
-        print("Speed:", att["speed"])
-        print("MTU:", att["mtu"])
-        print("-" * 50)
+        print(f"{att["dn"]:50} {att["descr"]:20} {att["speed"]:8} {att["mtu"]:6}")
